3f86ee0d3b356c66f7b18baa13bab510b8c15e0e
phikko <skeezze@gmail.com>
2021-12-18 23:29:12 +0100

    added tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-  Projekt
+  <h1 class="text-2xl font-bold text-blue-500">
+    Projekt
+  </h1>
 </template>
+
+<style lang=postcss>
+  @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
+</style>
