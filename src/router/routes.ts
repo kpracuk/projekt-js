e19ab7ef371cc201e52192a7b06f7c0186b83b5b
phikko <skeezze@gmail.com>
2021-12-19 17:41:31 +0100

    added register view

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,4 +1,5 @@
 export default [
   { path: '/', component: () => import('../views/Home.vue') },
   { path: '/login', component: () => import('../views/Login.vue') },
+  { path: '/register', component: () => import('../views/Register.vue') },
 ]
---
+++ src/views/Register.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-full flex justify-center pt-8">
+    <div class="p-4 bg-gray-800 rounded-md w-96 max-w-full flex flex-col gap-y-2">
+      <FormInput id="name" v-model="data.name">Imię i Nazwisko</FormInput>
+      <FormInput id="email" v-model="data.email">Email</FormInput>
+      <FormInput id="password" v-model="data.password">Hasło</FormInput>
+      <FormInput id="password-confirmation" v-model="data.passwordConfirmation">Potwierdź Hasło</FormInput>
+      <UiButton>Stwórz konto</UiButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { reactive } from 'vue';
+  import FormInput from '../components/form/FormInput.vue'
+  import UiButton from '../components/ui/UiButton.vue'
+
+  const data = reactive({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  })
+</script>
