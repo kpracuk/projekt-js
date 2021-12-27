12f97a2b15b7821d58d9347b888950c47752bbb8
kpracuk <skeezze@gmail.com>
2021-12-27 20:46:38 +0100

    added input error handling

--- src/components/form/FormInput.vue
+++ src/components/form/FormInput.vue
@@ -1,31 +1,41 @@
 <template>
   <div class="flex flex-col">
-    <label :for="id" class="text-sm text-blue-500 font-bold">
+    <label :for="id" class="text-sm text-blue-500 font-bold" :class="{ 'text-red-500': errors.length > 0 }">
       <slot />
     </label>
     <input
         v-bind="$attrs"
         :id="id"
         class="p-2 border-2 border-gray-500 focus:border-blue-500 transition bg-gray-900 text-white rounded-md focus:outline-none"
+        :class="{ 'border-red-500': errors.length > 0 }"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
     />
+    <div class="flex flex-col text-red-500 font-bold text-xs" v-if="errors.length > 0">
+      <span v-for="(error, key) in errors" :key="key">{{ error }}</span>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-  const props = defineProps({
-    modelValue: {
-      type: String,
-      default: () => ''
-    },
-    id: {
-      type: String,
-      required: true
-    }
-  })
+import {onMounted, reactive, watch} from "vue";
 
-  const emits = defineEmits([
-    'update:modelValue'
-  ])
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: () => ''
+  },
+  id: {
+    type: String,
+    required: true
+  },
+  errors: {
+    type: Array,
+    default: () => []
+  }
+})
+
+const emits = defineEmits([
+  'update:modelValue'
+])
 </script>
