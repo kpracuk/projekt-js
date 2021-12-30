0236810d9185e7c71f45f32014cf16ca6f3bec46
kpracuk <skeezze@gmail.com>
2021-12-30 22:17:18 +0100

    added disabled button state

--- src/components/ui/UiButton.vue
+++ src/components/ui/UiButton.vue
@@ -1,5 +1,19 @@
 <template>
-  <button class="px-4 py-2 text-white bg-blue-500 font-bold hover:bg-blue-600 rounded-md transition">
+  <button
+    v-bind="$attrs"
+    class="px-4 py-2 text-white font-bold rounded-md transition"
+    :class="[disabled ? 'bg-gray-600' : 'bg-blue-500 hover:bg-blue-600']"
+    :disabled="disabled"
+  >
     <slot />
   </button>
 </template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: () => false
+  }
+})
+</script>
